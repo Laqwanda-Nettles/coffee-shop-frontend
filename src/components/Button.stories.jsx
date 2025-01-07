1966ebeeb19465d70d6694d090bfc38f39595eb4
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-06 20:34:42 -0600

    created a button component + tested it using storybook

---
+++ src/components/Button.stories.jsx
@@ -0,0 +1,19 @@
+import Button from './Button';
+
+const meta = {
+  component: Button,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    label: "Add to Cart"
+  }
+};
+
+export const Order = {
+  args: {
+    label: "Order Now"
+  }
+};
