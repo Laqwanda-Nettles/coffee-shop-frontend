78496dcefb23b749c46a22285dc9704acbac686a
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-11 09:11:14 -0600

    updated tailwind styling for title and prices

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -3,14 +3,14 @@ import Button from "./Button";
 
 export default function ProductCard({ product, handleClick }) {
   return (
-    <div className="card bg-base-100 w-96 shadow-xl">
+    <div className="card bg-white w-96 shadow-lg">
       <figure className="px-10 pt-10">
         <img src={product.imageUrl} alt={product.name} className="rounded-xl" />
       </figure>
       <div className="card-body items-center text-center">
         <h2 className="card-title">{product.name}</h2>
         <p>{product.description}</p>
-        <p>{product.price}</p>
+        <p className="text-lg font-semibold">{product.price}</p>
         <div className="card-actions">
           <Button label={"Add to Cart"} handleClick={handleClick} />
         </div>
