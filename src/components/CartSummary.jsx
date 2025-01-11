8231fc94a7f60b95e1476302bb3c10d3736ec86e
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-11 09:06:59 -0600

    created a basic cartsummary table and tested it with storybook

--- src/components/CartSummary.jsx
+++ src/components/CartSummary.jsx
@@ -1,15 +1,31 @@
-import PropTypes from "prop-types";
-
-export default function CartSummary({ img, title }) {
+export default function CartSummary() {
   return (
-    <>
-      <div>Cart Summary Component {title}</div>
-      <div>{img}</div>
-    </>
+    <div className="px-6 mx-5">
+      <div className="text-3xl font-semibold text-center mb-4">
+        <h2>Order Summary</h2>
+      </div>
+      <table className="table text-xl">
+        <tbody>
+          <tr>
+            <td>Sub total</td>
+            <td>$7.00</td>
+          </tr>
+          <tr>
+            <td>Discount</td>
+            <td>$0.00</td>
+          </tr>
+          <tr>
+            <td>Tax</td>
+            <td>$2.54</td>
+          </tr>
+        </tbody>
+        <tfoot className="text-2xl">
+          <tr>
+            <td>Total</td>
+            <td>$9.54</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
   );
 }
-
-CartSummary.propTypes = {
-  img: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-};
