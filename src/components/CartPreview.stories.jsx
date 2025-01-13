f2de6f8b443ffead1d6c50cb5b162829fa00dd10
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-13 10:06:25 -0600

    added a CartPreview component

---
+++ src/components/CartPreview.stories.jsx
@@ -0,0 +1,16 @@
+import CartPreview from './CartPreview';
+
+const meta = {
+  component: CartPreview,
+};
+
+export default meta;
+
+export const Default = {
+  args: {
+    product: {
+      "name": "Espresso",
+      "imageUrl": "../../espresso.jpg"
+    }
+  }
+};
