d33fd364da92a9771839653b8573ee32b64867fa
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-16 13:09:40 -0600

    added link to individual product page when card is clicked

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,21 +1,28 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
+import Link from "next/link";
 
 export default function ProductCard({ product, handleClick }) {
   return (
-    <div className="card bg-white w-96 shadow-lg">
-      <figure className="px-10 pt-10">
-        <img src={product.imageUrl} alt={product.name} className="rounded-xl" />
-      </figure>
-      <div className="card-body items-center text-center">
-        <h2 className="card-title">{product.name}</h2>
-        <p>{product.description}</p>
-        <p className="text-lg font-semibold">{product.price}</p>
-        <div className="card-actions">
-          <Button label={"Add to Cart"} handleClick={handleClick} />
+    <Link href={`products/${product.id}`}>
+      <div className="card bg-white w-96 shadow-lg">
+        <figure className="px-10 pt-10">
+          <img
+            src={product.imageUrl}
+            alt={product.name}
+            className="rounded-xl"
+          />
+        </figure>
+        <div className="card-body items-center text-center">
+          <h2 className="card-title">{product.name}</h2>
+          <p>{product.description}</p>
+          <p className="text-lg font-semibold">{product.price}</p>
+          <div className="card-actions">
+            <Button label={"Add to Cart"} handleClick={handleClick} />
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
