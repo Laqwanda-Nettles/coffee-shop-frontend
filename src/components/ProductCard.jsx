0d09da97de3c1df62c7a8c76cc11e6943c6a3541
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-16 18:48:16 -0600

    added a conditional to link to view individual products.

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,28 +1,34 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function ProductCard({ product, handleClick }) {
+  const router = useRouter();
+  const currentPath = router.pathname;
+
   return (
-    <Link href={`products/${product.id}`}>
-      <div className="card bg-white w-96 shadow-lg">
-        <figure className="px-10 pt-10">
-          <img
-            src={product.imageUrl}
-            alt={product.name}
-            className="rounded-xl"
-          />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title">{product.name}</h2>
-          <p>{product.description}</p>
-          <p className="text-lg font-semibold">{product.price}</p>
-          <div className="card-actions">
-            <Button label={"Add to Cart"} handleClick={handleClick} />
-          </div>
+    <div className="card bg-white w-96 shadow-lg">
+      <figure className="px-10 pt-10">
+        <img src={product.imageUrl} alt={product.name} className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{product.name}</h2>
+        <p>{product.description}</p>
+        <p className="text-lg font-semibold">{product.price}</p>
+        {currentPath === "/products" && (
+          <Link
+            href={`products/${product.id}`}
+            className="italic text-info text-sm font-semibold"
+          >
+            View Item
+          </Link>
+        )}
+        <div className="card-actions">
+          <Button label={"Add to Cart"} handleClick={handleClick} />
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
 
