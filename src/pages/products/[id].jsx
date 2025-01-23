3040cb25692312c98fda57c6982c7ae8cc4bca90
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-22 19:26:57 -0600

    added custom hook and error handling

--- src/pages/products/[id].jsx
+++ src/pages/products/[id].jsx
@@ -1,13 +1,18 @@
 import { useRouter } from "next/router";
-import products from "../../mocks/products.json";
 import ProductCard from "@/components/ProductCard";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { useFetch } from "@/hooks/api";
+import Loading from "@/components/Loading";
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default function ProductPage() {
   const router = useRouter();
   const { id } = router.query; // Get 'id' from route params
-  const product = products[id] || {};
+
+  const url = `${BACKEND_URL}/products/${id}`;
+  const [error, loading, product] = useFetch(url, {});
 
   function handleAddToCart() {
     alert("Added Item to Cart: " + product.name);
@@ -20,7 +25,18 @@ export default function ProductPage() {
         <h1 className="text-4xl font-bold text-primary text-center mb-4">
           Product Page for product &#35; {id}
         </h1>
-        <ProductCard product={product} handleClick={handleAddToCart} />
+        {error ? (
+          <p className="text-red-400 text-lg text-center">
+            Error fetching product
+          </p>
+        ) : (
+          ""
+        )}
+        {loading ? (
+          <Loading />
+        ) : (
+          <ProductCard product={product} handleClick={handleAddToCart} />
+        )}
       </div>
       <Footer info={"Jazzed Up Coffee"} />
     </>
