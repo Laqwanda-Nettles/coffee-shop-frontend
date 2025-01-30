3a13961c4afd0209d122a1ee5a1e6109bfb5c7fc
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-29 19:46:42 -0600

    created a admin product card components that allows admin users to delete and edit a product

---
+++ src/components/AdminProductCard.jsx
@@ -0,0 +1,46 @@
+import PropTypes from "prop-types";
+import Button from "./Button";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+export default function AdminProductCard({
+  product,
+  handleClick,
+  secondaryHandleClick,
+}) {
+  const router = useRouter();
+  const currentPath = router.pathname;
+
+  return (
+    <div className="card bg-white w-96 shadow-lg">
+      <figure className="px-10 pt-10">
+        <img src={product.imageUrl} alt={product.name} className="rounded-xl" />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title">{product.name}</h2>
+        <p>{product.description}</p>
+        <p className="text-lg font-semibold">${product.price}</p>
+        {currentPath === "/products" && (
+          <Link
+            href={`products/${product._id}`}
+            className="italic text-info text-sm font-semibold"
+          >
+            View Item
+          </Link>
+        )}
+        <div className="card-actions">
+          <Button label={"Delete"} handleClick={handleClick} />
+          <Button
+            label={"Edit"}
+            handleClick={secondaryHandleClick}
+            variant="btn-info"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+AdminProductCard.propTypes = {
+  product: PropTypes.object.isRequired,
+};
