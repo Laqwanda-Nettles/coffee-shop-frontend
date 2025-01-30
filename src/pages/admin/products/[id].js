0a382e158007867d73a5f84e2105e47c316ceb1e
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-29 19:54:11 -0600

    imported navbar + footer

---
+++ src/pages/admin/products/[id].js
@@ -0,0 +1,11 @@
+import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
+
+export default function AdminProduct() {
+  return (
+    <>
+      <Navbar title={"Jazzed Up Coffee"} />
+      <Footer info={"Jazzed Up Coffee"} />
+    </>
+  );
+}
