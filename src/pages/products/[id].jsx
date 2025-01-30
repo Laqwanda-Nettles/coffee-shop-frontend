1c915a2d6a2a08d2555af73dbcece5d08de4cd77
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-30 11:34:32 -0600

    added fetching and removed custom useFetch hook

--- src/pages/products/[id].jsx
+++ src/pages/products/[id].jsx
@@ -2,8 +2,8 @@ import { useRouter } from "next/router";
 import ProductCard from "@/components/ProductCard";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import { useFetch } from "@/hooks/api";
 import Loading from "@/components/Loading";
+import { useEffect, useState } from "react";
 
 const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
@@ -11,10 +11,38 @@ export default function ProductPage() {
   const router = useRouter();
   const { id } = router.query; // Get 'id' from route params
 
-  const url = `${BACKEND_URL}/products/${id}`;
-  const [error, loading, product] = useFetch(url, {});
+  const [product, setProduct] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
+  const url = `${BACKEND_URL}/products/${id}`;
+
+  useEffect(() => {
+    if (!id) return;
+
+    async function fetchProduct() {
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error(`Failed to fetch product: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        setProduct(data);
+      } catch (error) {
+        console.error(error);
+        setError("Error fetching product.");
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProduct();
+  }, [id]);
   function handleAddToCart() {
+    if (!product) {
+      return;
+    }
     alert("Added Item to Cart: " + product.name);
   }
 
@@ -25,17 +53,13 @@ export default function ProductPage() {
         <h1 className="text-4xl font-bold text-primary text-center mb-4">
           Product Page for product &#35; {id}
         </h1>
-        {error ? (
-          <p className="text-red-400 text-lg text-center">
-            Error fetching product
-          </p>
-        ) : (
-          ""
-        )}
+        {error && <p className="text-red-400 text-lg text-center">{error}</p>}
         {loading ? (
           <Loading />
-        ) : (
+        ) : product ? (
           <ProductCard product={product} handleClick={handleAddToCart} />
+        ) : (
+          <p className="text-center text-xl text-red-400">Product not found.</p>
         )}
       </div>
       <Footer info={"Jazzed Up Coffee"} />
