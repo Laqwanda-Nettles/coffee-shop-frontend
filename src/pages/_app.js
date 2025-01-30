45d4e5c9abbb73dadc190b160d00242c2e23856d
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-30 11:32:53 -0600

    imported AuthProvider

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import { AuthProvider } from "@/context/AuthContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main>
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
+    </main>
+  );
 }
