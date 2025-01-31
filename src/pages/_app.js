4216d35cef548e20d478f34e1e29d22ada0d09ce
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-30 21:21:29 -0600

    setup + config Auth0

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,21 @@
 import { AuthProvider } from "@/context/AuthContext";
 import "@/styles/globals.css";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN;
+const CLIENTID = process.env.NEXT_PUBLIC_CLIENT_ID;
+const REDIRECT = process.env.NEXT_PUBLIC_REDIRECT_URL;
 
 export default function App({ Component, pageProps }) {
   return (
     <main>
-      <AuthProvider>
+      <Auth0Provider
+        domain={DOMAIN}
+        clientId={CLIENTID}
+        authorizationParams={{ redirect_uri: REDIRECT }}
+      >
         <Component {...pageProps} />
-      </AuthProvider>
+      </Auth0Provider>
     </main>
   );
 }
