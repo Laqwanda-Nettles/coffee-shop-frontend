76ed85fcf44db6ee70a12cb9b15213bc9836a7ae
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-30 23:37:05 -0600

    added user avatar with user.picture from Auth0 context provider

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -128,25 +128,31 @@ export default function Navbar({ title }) {
             role="button"
             className="btn btn-ghost btn-circle avatar"
           >
-            <div className="w-10 rounded-full">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={40}
-                height={40}
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill="currentColor"
-                  d="M11 7c0 1.66-1.34 3-3 3S5 8.66 5 7s1.34-3 3-3s3 1.34 3 3"
-                ></path>
-                <path
-                  fill="currentColor"
-                  fillRule="evenodd"
-                  d="M16 8c0 4.42-3.58 8-8 8s-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8M4 13.75C4.16 13.484 5.71 11 7.99 11c2.27 0 3.83 2.49 3.99 2.75A6.98 6.98 0 0 0 14.99 8c0-3.87-3.13-7-7-7s-7 3.13-7 7c0 2.38 1.19 4.49 3.01 5.75"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
+            {isAuthenticated ? (
+              <div className="w-10 rounded-full">
+                <img src={user.picture} alt="user avatar" />
+              </div>
+            ) : (
+              <div className="w-10 rounded-full">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={40}
+                  height={40}
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M11 7c0 1.66-1.34 3-3 3S5 8.66 5 7s1.34-3 3-3s3 1.34 3 3"
+                  ></path>
+                  <path
+                    fill="currentColor"
+                    fillRule="evenodd"
+                    d="M16 8c0 4.42-3.58 8-8 8s-8-3.58-8-8s3.58-8 8-8s8 3.58 8 8M4 13.75C4.16 13.484 5.71 11 7.99 11c2.27 0 3.83 2.49 3.99 2.75A6.98 6.98 0 0 0 14.99 8c0-3.87-3.13-7-7-7s-7 3.13-7 7c0 2.38 1.19 4.49 3.01 5.75"
+                    clipRule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+            )}
           </div>
           <ul
             tabIndex={0}
