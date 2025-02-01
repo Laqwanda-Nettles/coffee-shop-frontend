0293fa79a9bdb36e59a958ee7470b6f86839559a
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-02-01 09:27:52 -0600

    imported + implemented a Quantity Button feature

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,17 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
+import { useEffect, useState } from "react";
+import QuantityBtn from "./QuantityBtn";
 
-export default function CartItem({ product, handleClick }) {
+export default function CartItem({ product, onQuantityChange, handleRemove }) {
+  const [quantity, setQuantity] = useState(product.quantity || 1);
+
+  useEffect(() => {
+    onQuantityChange(product._id, quantity);
+  }, [quantity]);
+
+  const totalPrice = product.price * quantity;
+
   return (
     <div className="card card-side bg-base-100 shadow-xl">
       <figure className="max-w-64 max-h-64">
@@ -10,12 +20,15 @@ export default function CartItem({ product, handleClick }) {
       <div className="card-body">
         <h2 className="card-title font-semibold text-2xl">{product.name}</h2>
         <p className="italic text-gray-700 text-lg">{product.category}</p>
-        <p className="text-xl font-semibold">${product.price}</p>
-        <div className="card-actions justify-end">
+        <div className="flex items-center gap-4">
+          <p className="text-xl font-semibold">${totalPrice.toFixed(2)}</p>
+          <QuantityBtn quantity={quantity} onQuantityChange={setQuantity} />
+        </div>
+        <div className="card-actions justify-end mt-2">
           <Button
             className="btn btn-outline btn-error"
             label={"Remove"}
-            handleClick={handleClick}
+            handleClick={() => handleRemove(product._id)}
             variant="btn-outline btn-error"
           />
         </div>
