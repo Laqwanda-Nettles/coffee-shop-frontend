5a9714c263e25c44408501120dbba93edfd1f3e7
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-02-05 11:24:02 -0600

    added tailwind styling to adujst footer when content is minimum

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -14,37 +14,39 @@ export default function Cart() {
   }
 
   return (
-    <>
+    <div className="flex flex-col">
       <Navbar title={"Jazzed Up Coffee"} />
-      <div className="mt-4 mb-5 border-b-2 border-neutral pb-4">
-        <h1 className="text-center text-4xl text-primary font-bold">
-          Shopping Cart
-        </h1>
-      </div>
-      <div className="flex flex-col gap-5 justify-evenly items-center">
-        {cart.length === 0 ? (
-          <p className="text-center text-info text-xl font-semibold">
-            Your cart is empty.
-          </p>
-        ) : (
-          cart.map((item) => (
-            <CartItem
-              key={item._id}
-              product={item}
-              onQuantityChange={updateItemQuantity}
-              handleRemove={() => removeItemFromCart(item._id)}
-            />
-          ))
-        )}
-      </div>
-      <div className="m-4 text-center">
-        <Button
-          label={"Checkout"}
-          handleClick={handleCheckout}
-          variant="btn-info btn-wide"
-        />
+      <div className={`min-h-[84vh]`}>
+        <div className="mt-4 mb-5 border-b-2 border-neutral pb-4">
+          <h1 className="text-center text-4xl text-primary font-bold">
+            Shopping Cart
+          </h1>
+        </div>
+        <div className="flex flex-col gap-5 justify-evenly items-center">
+          {cart.length === 0 ? (
+            <p className="text-center text-info text-xl font-semibold">
+              Your cart is empty.
+            </p>
+          ) : (
+            cart.map((item) => (
+              <CartItem
+                key={item._id}
+                product={item}
+                onQuantityChange={updateItemQuantity}
+                handleRemove={() => removeItemFromCart(item._id)}
+              />
+            ))
+          )}
+        </div>
+        <div className="m-4 text-center">
+          <Button
+            label={"Checkout"}
+            handleClick={handleCheckout}
+            variant="btn-info btn-wide"
+          />
+        </div>
       </div>
       <Footer info={"Jazzed Up Coffee"} />
-    </>
+    </div>
   );
 }
