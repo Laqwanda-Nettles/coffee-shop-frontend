a989802362ff5a0c56ae45cf4d88ca9ef509df8d
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-02-06 00:09:49 -0600

    utilized state and daisyui to handle alert messages

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -2,15 +2,25 @@ import CartSummary from "@/components/CartSummary";
 import CheckoutForm from "@/components/CheckoutForm";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
+import { useState } from "react";
 
 export default function Checkout() {
+  const [alert, setAlert] = useState(false);
+  const [userName, setUserName] = useState("");
   function handlePayment(name, email, address) {
-    alert(`Thank You ${name}, for your purchase!`);
+    setUserName(name);
+    setAlert(true);
+    setTimeout(() => setAlert(false), 3000);
   }
 
   return (
     <>
       <Navbar title={"Jazzed Up Coffee"} />
+      {alert && (
+        <div className="alert alert-success m-4 text-lg font-semibold">
+          <span>{`Thank You ${userName}, for your purchase!`}</span>
+        </div>
+      )}
       <div className={`min-h-[84vh]`}>
         <div className="m-4 flex justify-evenly items-start gap-4">
           <CheckoutForm handleClick={handlePayment} />
